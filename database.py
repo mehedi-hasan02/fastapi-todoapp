8bc0592c27b866e15fcccc36695f106e1aef6ab8
mehedi-hasan02 <mehedisani02@gmail.com>
2026-08-18 22:02:38 +0600

    my todo app

--- database.py
+++ database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345@localhost/todoapplication_db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345@localhost/todoapplication_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
